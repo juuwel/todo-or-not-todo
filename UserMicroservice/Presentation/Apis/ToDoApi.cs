aaaa8c1ac6e8f2d909fd2faadf5e1fa4156252fc
MatejMa2ur <matejmazur@outlook.com>
2025-04-08 12:38:46 +0200

    feat: add UserMicroservice with foundational setup
    - Initialize UserMicroservice project with .NET 9.0.
    - Add necessary configuration files (appsettings, Dockerfile, etc.).
    - Configure OpenAPI and basic project structure.
    
    This commit adds a basic UserMicroservice project with support for .NET 9.0, OpenAPI, and Docker configuration. The project includes initial domain, infrastructure, and presentation layers with placeholder files for future development.

---
+++ UserMicroservice/Presentation/Apis/ToDoApi.cs
@@ -0,0 +1,46 @@
+﻿namespace ToDoBackend.Presentation.Apis;
+
+public static class ToDoApi
+{
+    public static RouteGroupBuilder AddTaskApi(this IEndpointRouteBuilder app)
+    {
+        var api = app.MapGroup("/api/v1/tasks");
+
+        /*api.MapGet("/{subscriptionType}", GetSubscribers)
+            .WithName("Get all subscribers for a subscription type");*/
+
+        return api;
+    }
+    
+    /*private static async Task<Results<Ok<GetSubscriptionDto>, ProblemHttpResult>> CreateSubscription(
+        [FromBody] CreateSubscriptionDto request, [FromServices] ISubscriberService subscriberService,
+        CancellationToken cancellationToken)
+    {
+        try
+        {
+            var result = await subscriberService.SubscribeAsync(request);
+            var dto = new GetSubscriptionDto
+            {
+                Id = result.Id,
+                Email = result.Subscriber!.Email,
+                SubscriptionType = result.SubscriptionType!.Type
+            };
+            return TypedResults.Ok(dto);
+        }
+        catch (NotFoundException e)
+        {
+            Log.Error("Subscription type not found: {SubscriptionType}", request.SubscriptionType);
+            return TypedResults.Problem(e.Message, statusCode: StatusCodes.Status404NotFound);
+        }
+        catch (BadRequestException e)
+        {
+            Log.Error("Bad request: {Error}", e.Message);
+            return TypedResults.Problem(e.Message, statusCode: StatusCodes.Status400BadRequest);
+        } 
+        catch (Exception e)
+        {
+            Log.Error("An error occurred while creating subscription: {Error}", e.Message);
+            return TypedResults.Problem(e.Message, statusCode: StatusCodes.Status500InternalServerError);
+        }
+    }*/
+}
