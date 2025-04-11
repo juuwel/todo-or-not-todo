39e4acba07698b4c5805d7fa0d79665fe62fb2cf
Julia Ilasova <1julka1il@gmail.com>
2025-04-11 13:09:12 +0200

    simple log in page (static)

--- ToDoFrontend/src/pages/login-page/login-page.component.ts
+++ ToDoFrontend/src/pages/login-page/login-page.component.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './login-page.component.html',
   styles: ``
 })
 export class LoginPageComponent {
+  protected isRegistering = false;
+  protected loginForm: FormGroup;
 
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  public toggleRegistering() {
+    this.isRegistering = !this.isRegistering;
+  }
+
+  public onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Form Submitted', this.loginForm.value);
+    } else {
+      console.log('Form is invalid');
+    }
+  }
 }
