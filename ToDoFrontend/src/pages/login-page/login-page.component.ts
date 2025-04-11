64885f3102131297fa10e14a1b8ff905751dabff
Julia Ilasova <1julka1il@gmail.com>
2025-04-11 14:02:33 +0200

    hook up auth to BE - wip

--- ToDoFrontend/src/pages/login-page/login-page.component.ts
+++ ToDoFrontend/src/pages/login-page/login-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {AuthService} from '../../services/auth.service';
 
 @Component({
   selector: 'app-login-page',
@@ -12,9 +13,9 @@ import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/
 export class LoginPageComponent {
   protected isRegistering = false;
   protected loginForm: FormGroup;
+  protected errorMessage = '';
 
-
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, public authService: AuthService) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required]
@@ -25,11 +26,34 @@ export class LoginPageComponent {
     this.isRegistering = !this.isRegistering;
   }
 
-  public onSubmit() {
+  public async onSubmit() {
     if (this.loginForm.valid) {
-      console.log('Form Submitted', this.loginForm.value);
+      if (this.isRegistering) {
+        await this.register();
+      } else {
+        await this.logIn();
+      }
     } else {
       console.log('Form is invalid');
     }
+  }
+
+  private async logIn() {
+    const email = this.loginForm.controls['email'].value;
+    const password = this.loginForm.controls['password'].value;
+    const success = await this.authService.logIn(email, password);
+
+    if (success) {
+      // TODO navigate to home
+      return;
+    }
+
+    this.errorMessage = "Incorrect email or password";
+  }
+
+  private async register() {
+    const email = this.loginForm.controls['email'].value;
+    const password = this.loginForm.controls['password'].value;
+    this.authService.register(email, password);
   }
 }
---
+++ ToDoFrontend/src/services/auth.service.ts
@@ -0,0 +1,45 @@
+﻿import {Injectable} from '@angular/core';
+import {HttpClient, HttpResponse} from '@angular/common/http';
+import {Observable, tap} from 'rxjs';
+
+interface AuthResponse {
+  userId: string;
+  email: string;
+  token: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private readonly baseUrl = "http://localhost:5242/auth";
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  public async logIn(username: string, password: string): Promise<boolean> {
+    return new Promise((resolve) => {
+      this.httpClient.post<AuthResponse>(`${this.baseUrl}/login`, {
+        Email: username,
+        Password: password,
+      }).pipe(
+        tap((res: AuthResponse) => {
+          localStorage.setItem('token', res.token);
+          resolve(true); // Resolve true on success
+        })
+      ).subscribe({
+        error: (err) => {
+          resolve(false); // Resolve false on error
+        }
+      });
+    });
+  }
+
+  public logOut() {
+
+  }
+
+  public register(username: string, password: string) {
+
+  }
+}
