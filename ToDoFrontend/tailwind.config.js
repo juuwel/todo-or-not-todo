557a8b18be40c5b9e3538041c124495688479f7a
Julia Ilasova <1julka1il@gmail.com>
2025-04-26 15:11:38 +0200

    add nicer color scheme and form validation to task form, final bug fixes

--- ToDoFrontend/tailwind.config.js
+++ ToDoFrontend/tailwind.config.js
@@ -6,14 +6,16 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: '#1DA1F2',
-        secondary: '#14171A',
-        accent: '#657786',
+        primary: '#7699D4',
+        secondary: '#63B4D1',
+        accent: '#90FCF9',
         neutral: '#AAB8C2',
         info: '#3ABFF8',
         success: '#36D399',
         warning: '#FBBD23',
-        error: '#F87272'
+        error: '#F87272',
+        background: '#EFF3FA',
+        background_accent: '#E0E8F5',
       }
     },
   },
